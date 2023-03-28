3db07e567530f028acd552f0622bc04a9152cb84
Cosmin <v.alexandrucosmin@gmail.com>
2023-03-28 06:56:38 -0400

    Updates

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,22 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses } = useContext(AppContext);
     const [currentBudget, setCurrentBudget] = useState(budget);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
 
     function modifyBudget(value) {
         if (value > 20000 ) {
-            alert("The value cannot exceed 20000");
+            alert("The budget cannot exceed 20000");
             setCurrentBudget(2000);
+            return;
+        }
+        if (currentBudget < totalExpenses) {
+            alert("The budget cannot be lower than the spending.");
+            setCurrentBudget(totalExpenses);
             return;
         }
         setCurrentBudget(value);
